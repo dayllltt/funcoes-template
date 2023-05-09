d1302c4e4dad4efc9711a38762f5953df3656528
dayllltt <131316375+dayllltt@users.noreply.github.com>
2023-05-08 21:28:58 -0300

    Add files via upload

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,9 @@
 // Resolva o Exercício de Fixação!
+const dadosPessoais = (_nome, anoNascimento , yearrrAtual) => {
+    return `${_nome}, você é maior de idade? ${yearrrAtual - anoNascimento >= 18}, você tem ${yearrAtual - anoNascimento}anos!`
+
+}
+
+console.log(dadosPessoais("day", 2000, 2023));
+console.log(dadosPessoais("TesttPersonn", 2009, 2023))
+console.log(dadosPessoais("TesttPerson2", 2003, 2023))
